5a1282e9945550a981912f8a15e3fdfd37e6d4fe
CamiloDev22 <kamilo042.cp@gmail.com>
2023-04-10 14:26:37 -0500

    añado archivos

---
+++ js/nav.js
@@ -0,0 +1,27 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+    <div class="nav">
+    <img src="img/logo1.png" class="brand-logo" alt="">
+    <div class="nav-items">
+        <div class="search">
+            <input type="text" class="search-box" placeholder="search brand, product">
+            <button class="search-btn">search</button>
+        </div>
+        <a href="#"><img src="img/user.png" alt=""></a>
+        <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+</div>
+<ul class="links-container">
+    <li class="link-item"><a href="#" class="link">HOME</a></li>
+    <li class="link-item"><a href="#" class="link">POPULAR</a></li>
+    <li class="link-item"><a href="#" class="link">NEW IN</a></li>
+    <li class="link-item"><a href="#" class="link">POSTERS</a></li>
+    <li class="link-item"><a href="#" class="link">GREETING CARDS</a></li>
+</ul>
+    `;
+
+}
+
+createNav();
